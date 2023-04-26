be4a27bc55bf91f5d015b8de379520b993c63bc7
matteopetrera <matteopetrera@localhost>
2023-04-26 14:48:04 +0200

    lez 26/04

--- src/main/java/it/polito/tdp/metroparis/Controller.java
+++ src/main/java/it/polito/tdp/metroparis/Controller.java
@@ -1,14 +1,18 @@
 package it.polito.tdp.metroparis;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.metroparis.model.Fermata;
+import it.polito.tdp.metroparis.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
 public class Controller {
-
+	private Model model;
     @FXML
     private ResourceBundle resources;
 
@@ -16,10 +20,10 @@ public class Controller {
     private URL location;
 
     @FXML
-    private ComboBox<?> boxArrivo;
+    private ComboBox<Fermata> boxArrivo;
 
     @FXML
-    private ComboBox<?> boxPartenza;
+    private ComboBox<Fermata> boxPartenza;
 
     @FXML
     private TextArea txtResult;
@@ -31,7 +35,10 @@ public class Controller {
 
     @FXML
     void handleCrea(ActionEvent event) {
-
+    	this.model.creaGrafo();
+    	if(this.model.isGraficoLoaded()==true) {
+    		txtResult.setText("grafo correttamente creato");
+    	}
     }
 
     @FXML
@@ -41,5 +48,13 @@ public class Controller {
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Metro.fxml'.";
 
     }
+
+	public void setModel(Model m) {
+		// TODO Auto-generated method stub
+		this.model = m;
+		List<Fermata> fermate = this.model.getAllFermate();
+		boxPartenza.getItems().setAll(fermate);
+		boxArrivo.getItems().setAll(fermate);
+	}
 
 }
--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.javadocmd.simplelatlng.LatLng;
 
@@ -26,7 +27,7 @@ public class MetroDAO {
 			ResultSet rs = st.executeQuery();
 
 			while (rs.next()) {
-				Fermata f = new Fermata(rs.getInt("id_Fermata"), rs.getString("nome"),
+				Fermata f = new Fermata(rs.getInt("id_fermata"), rs.getString("nome"),
 						new LatLng(rs.getDouble("coordx"), rs.getDouble("coordy")));
 				fermate.add(f);
 			}
@@ -68,7 +69,112 @@ public class MetroDAO {
 
 		return linee;
 	}
-
 	
+	
+	public boolean isConnesse(Fermata partenza, Fermata arrivo) {
+		
+		String sql = "SELECT COUNT(*) AS c "
+				+ "FROM connessione "
+				+ "WHERE id_stazP=? "
+				+ "AND id_stazA=?";
+		
+		try {
+			
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			st.setInt(2, arrivo.getIdFermata());
+			
+			ResultSet res = st.executeQuery();
+			
+			res.first();
+			
+			int c = res.getInt("c");
+			
+			conn.close();
+			
+			return c != 0;
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
 
+	public List<Fermata> trovaCollegate(Fermata partenza) {
+		// TODO Auto-generated method stub
+		
+		String sql = "SELECT * "
+				+ "FROM fermata "
+				+ "WHERE id_fermata IN( "
+				+ "SELECT id_stazA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP=? "
+				+ "GROUP BY id_stazA "
+				+ ")ORDER BY nome ASC";
+		
+		List<Fermata> fermate = new ArrayList<>();
+		
+		try {
+			
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			
+			
+			ResultSet res = st.executeQuery();
+			
+			while(res.next()) {
+				Fermata f = new Fermata(res.getInt("id_fermata"), res.getString("nome"),
+						new LatLng(res.getDouble("coordx"), res.getDouble("coordy")));
+				fermate.add(f);
+			}
+			st.close();
+			conn.close();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+		return fermate;
+	}
+	
+	public List<Fermata> trovaIdCollegate(Fermata partenza, Map<Integer, Fermata> fermateIdMap ) {
+		String sql = "SELECT id_stazA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "GROUP BY id_stazA ";
+		
+		List<Fermata> fermate = new ArrayList<>();
+		
+		try {
+			Connection conn = DBConnect.getConnection() ;
+			
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			st.setInt(1, partenza.getIdFermata());
+			
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {				
+				Integer idFermata = res.getInt("id_stazA");
+				fermate.add(fermateIdMap.get(idFermata));
+			}
+			
+			st.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} 
+
+		return fermate;
+	}
 }
